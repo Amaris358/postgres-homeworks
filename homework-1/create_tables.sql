040b60e217c1500fd5bd68f8713aa4f32bebc045
Anastasiya <puchinina.anastasia58@gmail.com>
2024-02-01 01:42:30 +0700

    В файле main.py написан скрипт, который заполняет созданные в pgAdmin таблицы из файлов

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(25) NOT NULL,
+	last_name varchar(35) NOT NULL,
+	titlte varchar(50) NOT NULL,
+	birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(50) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+	contact_name varchar(70) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+from pathlib import Path
+
+CUSTOMERS_FILE = Path(__file__).parent.joinpath("north_data", "customers_data.csv")
+EMPLOYEES_FILE = Path(__file__).parent.joinpath("north_data", "employees_data.csv")
+ORDERS_FILE = Path(__file__).parent.joinpath("north_data", "orders_data.csv")
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+with conn.cursor() as cur:
+    with open(CUSTOMERS_FILE, "r") as file:
+        all_data = csv.reader(file)
+        list_customers = list(all_data)
+        del list_customers[0]
+        for row in list_customers:
+            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", tuple(row))
+    with open(EMPLOYEES_FILE, "r") as file:
+        all_data = csv.reader(file)
+        list_employees = list(all_data)
+        del list_employees[0]
+        for row in list_employees:
+            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", tuple(row))
+    with open(ORDERS_FILE, "r") as file:
+        all_data = csv.reader(file)
+        list_orders = list(all_data)
+        del list_orders[0]
+        for row in list_orders:
+            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", tuple(row))
+    conn.commit()
